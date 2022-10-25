1edd6c336fa760ecff32565f17c0e478a3863b07
Elijah Gundorin <elijahgundorin@Elijahs-MBP.home>
2022-10-25 10:37:16 -0400

    Working on Post data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import ThoughtsPage from "./pages/thoughts/thoughts-page";
+import SearchPage from "./pages/search/search-page";
+import CreatePage from "./pages/create/create-page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="/" element={<ThoughtsPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="create" element={<CreatePage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
