dbe9c989ec3832f188077d205e800988a897b536
Elijah Gundorin <elijahgundorin@Elijahs-MacBook-Pro.local>
2022-10-28 15:34:34 -0400

    CSS grid added for card layout

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ thought }) => {
   const { id, text, title } = thought;
+  const subString = text.substring(0,25) + '...'
   return (
     <div className='card'>
         <h2 className='card-title'>{title}</h2>
-        <p className='card-text'>{text}</p>
+        <p className='card-text'>{subString}</p>
         <Link className='lnk-btn card-btn' to={`/thoughts/${id}`}>Explore Thought</Link>
     </div>
   )
